28025dc266e08a5080d33577e642e908861a700d
WishaEman <eman.wisha@gmail.com>
2023-07-26 13:36:32 +0500

    Add changes in url and views

--- django_API/APIApp/urls.py
+++ django_API/APIApp/urls.py
@@ -1,15 +1,11 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from .views import UserViewSet, GroupViewSet
 
 app_name = 'APIApp'
 
-# router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
-# router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
-    path('user', UserViewSet.as_view(), name='user'),
-    path('group', GroupViewSet.as_view(), name='group'),
+    path('user/', UserViewSet.as_view(), name='user'),
+    path('group/', GroupViewSet.as_view(), name='group'),
 ]
 
--- django_API/APIApp/views.py
+++ django_API/APIApp/views.py
@@ -1,22 +1,19 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from .serializers import UserSerializer, GroupSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(ListCreateAPIView):
     """
         API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('date_joined')
     serializer_class = UserSerializer
-    # permission_classes = [permissions.IsAuthenticated]
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(ListCreateAPIView):
     """
         API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    # permission_classes = [permissions.IsAuthenticated]
